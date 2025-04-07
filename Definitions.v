4356b0e09fa3e3dc62ca3e040667104da347ccbc
Crenchi <christopherlambrinos@gmail.com>
2025-04-07 10:54:03 +0200

    added emptytree, isempty and height, draft of query and build

--- Definitions.v
+++ Definitions.v
@@ -1,5 +1,8 @@
 Require Import Nat.
 Require Import List.
+Require Import Bool.
+Require Import Datatypes.
+
 Import ListNotations.
 
 (* Generic segment tree definition *)
@@ -9,8 +12,24 @@ Inductive Segtree : Type :=
 .
 
 (* Core functions for a segment tree *)
-Fixpoint query (t : Segtree) (lbound rbound : nat) : nat.
-Admitted.
+(* These functions pertain to summation *)
+Fixpoint query (t : Segtree) (tl tr : nat) : option nat :=
+    match t with
+    | Empty => None
+    | Node l value lbound rbound r =>
+    (* 3 cases, case 1: no overlap *)
+    if (tl <? lbound) || (rbound <? tr) then None
+    (* Case 2: full overlap *)
+    else if (tl <=? lbound) && (rbound <=? tr) then Some value
+    (* Case 3: Partial overlap — combine results *)
+    else
+      match (query l tl tr, query r tl tr) with
+      | (Some v1, Some v2) => Some (v1 + v2)
+      | (Some v, None) | (None, Some v) => Some v
+      | (None, None) => None
+      end
+end.
+
 
 (*
 Following two functions define different types of segment tress 
@@ -23,4 +42,30 @@ Fixpoint update (t : Segtree) (lbound rbound value : nat) : Segtree.
 Admitted.
 
 Fixpoint build (array : list nat) (lbound rbound : nat) : Segtree.
+(*
+        match lbound =? rbound with
+    | true => 
+        match nth_error array lbound with
+        | Some v => Node Empty v lbound rbound Empty
+        | None => Empty
+        end
+*)
 Admitted.
+
+(* Supplementary functions to a segment tree *)
+
+Definition emptyTree : Segtree := Empty.
+
+Definition isEmpty (t : Segtree) : bool :=
+    match t with
+    | Empty => true
+    | _     => false
+    end
+.
+
+Fixpoint height (t : Segtree) : nat :=
+    match t with
+    | Empty  => 0
+    | Node l _ _ _ _ => 1 + (height l)
+    end
+.
